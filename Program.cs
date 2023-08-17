994079aed8f0117a28994cb3411a62e058973539
Wagner Nunes <wagnersgoncalves@hotmail.com>
2023-08-17 15:29:04 -0300

    st commit

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("This is a simple C# program!");
+Console.WriteLine("Press any key to exit.");
+Console.ReadLine();
